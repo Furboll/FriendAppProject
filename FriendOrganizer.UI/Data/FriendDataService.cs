264c4b3206193c4460d65d6028c93589f6e6015d
Gior the Furball <zero.furball@gmail.com>
2017-10-29 13:26:55 +0100

    UI shenanigans

---
+++ FriendOrganizer.UI/Data/FriendDataService.cs
@@ -0,0 +1,20 @@
+﻿using FriendOrganizer.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FriendOrganizer.UI.Data
+{
+    public class FriendDataService : IFriendDataService
+    {
+        public IEnumerable<Friend> GetALL()
+        {
+            yield return new Friend { FirstName = "Ryu", LastName = "Bloodfist" };
+            yield return new Friend { FirstName = "Drisse", LastName = "Silverwind" };
+            yield return new Friend { FirstName = "Raiko",  LastName = "Earthshaker" };
+            yield return new Friend { FirstName = "Bhissy", LastName = "Stormtotem" };
+        }
+    }
+}
