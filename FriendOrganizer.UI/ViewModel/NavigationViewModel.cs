d5cae00aed319054bda5b97c5d8daf328107b911
Gior the Furball <zero.furball@gmail.com>
2017-10-29 15:37:40 +0100

    Added Prism Core NugetPackage for event handling in the UI.

--- FriendOrganizer.UI/ViewModel/NavigationViewModel.cs
+++ FriendOrganizer.UI/ViewModel/NavigationViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using FriendOrganizer.Model;
 using FriendOrganizer.UI.Data;
+using FriendOrganizer.UI.Event;
+using Prism.Events;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -9,14 +11,16 @@ using System.Threading.Tasks;
 
 namespace FriendOrganizer.UI.ViewModel
 {
-    public class NavigationViewModel : INavigationViewModel
+    public class NavigationViewModel : ViewModelBase, INavigationViewModel
     {
         private IFriendLookupDataService _friendLookupService;
+        private IEventAggregator _eventAggregator;
 
-        public NavigationViewModel(IFriendLookupDataService friendLookupService)
+        public NavigationViewModel(IFriendLookupDataService friendLookupService, IEventAggregator eventAggregator)
         {
             _friendLookupService = friendLookupService;
             Friends = new ObservableCollection<LookupItem>();
+            _eventAggregator = eventAggregator;
         }
 
         public async Task LoadAsync()
@@ -30,5 +34,22 @@ namespace FriendOrganizer.UI.ViewModel
         }
 
         public ObservableCollection<LookupItem> Friends { get; }
+
+        private LookupItem _selectedFriend;
+
+        public LookupItem SelectedFriend
+        {
+            get { return _selectedFriend; }
+            set
+            {
+                _selectedFriend = value;
+                OnPropertyChanged();
+                if (_selectedFriend != null)
+                {
+                    _eventAggregator.GetEvent<OpenFriendDetailViewEvent>().Publish(_selectedFriend.Id);
+                }
+            }
+        }
+
     }
 }
